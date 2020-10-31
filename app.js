3692da8cb243f97ab501bb354e8ba96bfa328f44
pedro <pedromf34@outlook.com>
2020-10-31 17:07:25 -0300

    implementando webtoken

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import home from './src/routes/home';
 import user from './src/routes/user';
+import token from './src/routes/token';
 
 import './src/database';
 
@@ -22,6 +23,7 @@ class App {
   routes() {
     this.app.use('/', home);
     this.app.use('/users', user);
+    this.app.use('/tokens', token);
   }
 }
 
